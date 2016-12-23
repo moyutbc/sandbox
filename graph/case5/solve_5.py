c1791924f7533ed91a0c5f2fce2cff8ff96225b4
moyutbc <tourokuyou888@gmail.com>
2016-12-23 16:34:21 +0900

    modified solve_5.py for changing directory sturct

--- graph/case5/solve_5.py
+++ graph/case5/solve_5.py
@@ -90,20 +90,38 @@ def solve_case(f00, fc, file_in, x_in):
 
 if __name__ == '__main__':
     
-    file_f = ['f22.csv', 'f23.csv', 'f24.csv', 'f25.csv',
-              'f34.csv', 'f34.csv', 'f35.csv',
-              'f44.csv', 'f45.csv',
-              'f55.csv']
-    
-    solve_case('f0_01.csv', 'fc_01.csv', file_f, 'x_01.csv')
-    solve_case('f0_02.csv', 'fc_02.csv', file_f, 'x_02.csv')
-    solve_case('f0_03.csv', 'fc_03.csv', file_f, 'x_03.csv')
-    solve_case('f0_04.csv', 'fc_04.csv', file_f, 'x_04.csv')
-    solve_case('f0_05.csv', 'fc_05.csv', file_f, 'x_05.csv')
-    solve_case('f0_06.csv', 'fc_06.csv', file_f, 'x_06.csv')
-    solve_case('f0_07.csv', 'fc_07.csv', file_f, 'x_07.csv')
-    solve_case('f0_08.csv', 'fc_08.csv', file_f, 'x_08.csv')
-    solve_case('f0_09.csv', 'fc_09.csv', file_f, 'x_09.csv')
-    solve_case('f0_10.csv', 'fc_10.csv', file_f, 'x_10.csv')
+    file_special = [['case5-01/', 'f0_01.csv', 'fc_01.csv', 'x_01.csv'],
+                    ['case5-02/', 'f0_02.csv', 'fc_02.csv', 'x_02.csv'],
+                    ['case5-03/', 'f0_03.csv', 'fc_03.csv', 'x_03.csv'],
+                    ['case5-04/', 'f0_04.csv', 'fc_04.csv', 'x_04.csv'],
+                    ['case5-05/', 'f0_05.csv', 'fc_05.csv', 'x_05.csv'],
+                    ['case5-06/', 'f0_06.csv', 'fc_06.csv', 'x_06.csv'],
+                    ['case5-07/', 'f0_07.csv', 'fc_07.csv', 'x_07.csv'],
+                    ['case5-08/', 'f0_08.csv', 'fc_08.csv', 'x_08.csv'],
+                    ['case5-09/', 'f0_09.csv', 'fc_09.csv', 'x_09.csv'],
+                    ['case5-10/', 'f0_10.csv', 'fc_10.csv', 'x_10.csv'],
+                    ['case5-11/', 'f0_11.csv', 'fc_11.csv', 'x_11.csv'],
+                    ['case5-12/', 'f0_12.csv', 'fc_12.csv', 'x_12.csv'],
+                    ['case5-13/', 'f0_13.csv', 'fc_13.csv', 'x_13.csv'],
+                    ['case5-14/', 'f0_14.csv', 'fc_14.csv', 'x_14.csv'],
+                    ['case5-15/', 'f0_15.csv', 'fc_15.csv', 'x_15.csv'],
+                    ['case5-16/', 'f0_16.csv', 'fc_16.csv', 'x_16.csv'],
+                    ['case5-17/', 'f0_17.csv', 'fc_17.csv', 'x_17.csv'],
+                    ['case5-18/', 'f0_18.csv', 'fc_18.csv', 'x_18.csv'],
+                    ['case5-19/', 'f0_19.csv', 'fc_19.csv', 'x_19.csv'],
+                    ['case5-20/', 'f0_20.csv', 'fc_20.csv', 'x_20.csv'],
+                    ['case5-21/', 'f0_21.csv', 'fc_21.csv', 'x_21.csv']]
 
+    file_common = ['common/f22.csv',
+                   'common/f23.csv',
+                   'common/f24.csv',
+                   'common/f25.csv',
+                   'common/f34.csv',
+                   'common/f34.csv',
+                   'common/f35.csv',
+                   'common/f44.csv',
+                   'common/f45.csv',
+                   'common/f55.csv']
 
+    for li in file_special:
+        solve_case(li[0] + li[1], li[0] + li[2], file_common, li[0] + li[3])
