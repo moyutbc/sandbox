cf98772c1086babb1b2fd04da3c3edd4ecee1220
moyutbc <tourokuyou888@gmail.com>
2017-02-01 22:32:23 +0900

    added answer

--- graph/drawing/relate.py
+++ graph/drawing/relate.py
@@ -4,6 +4,7 @@ import sys
 from pprint import pprint
 from sympy import solve, symbols, Rational, sqrt
 import numpy as np
+import logging
 
 def load_length(csvfilename):
     arr = np.empty((0,5), float)
@@ -22,10 +23,10 @@ def get_formula_with_zero(co_list):
     return f
 
 def is_valid_length(l):
-    return True
-    rtn = False
-    if l % 0.25 == 0:
-        rtn = True
+    rtn = True 
+    if isinstance(l, float) and l % 0.0625 != 0:
+        rtn = False
+    logging.debug('%s is %s' % (l, rtn))
     return rtn
 
 
@@ -42,6 +43,13 @@ def get_formula_with_length(co_list, len_arr):
             f.append(tmp)
     return f
 
+def is_valid_inner_product(ip):
+    return True
+    rtn = False
+    if ip % 0.125 == 0:
+        rtn = True
+    return rtn
+
 def get_formula_with_inner_product(co_list, ip_list):
     ax, ay, az = co_list[0] 
     bx, by, bz = co_list[1]
@@ -50,26 +58,25 @@ def get_formula_with_inner_product(co_list, ip_list):
     ex, ey, ez = co_list[4]
     f = []
 
-    N = 0.025
-    if ip_list['ip22'] % N == 0:
+    if is_valid_inner_product(ip_list['ip22']) == 0:
         f.append(bx * bx + by * by + bz * bz - ip_list['ip22'])
-    if ip_list['ip23'] % N == 0:
+    if is_valid_inner_product(ip_list['ip23']) == 0:
         f.append(bx * cx + by * cy + bz * cz - ip_list['ip23'])
-    if ip_list['ip24'] % N == 0:
+    if is_valid_inner_product(ip_list['ip24']) == 0:
         f.append(bx * dx + by * dy + bz * dz - ip_list['ip24'])
-    if ip_list['ip25'] % N == 0:
+    if is_valid_inner_product(ip_list['ip25']) == 0:
         f.append(bx * ex + by * ey + bz * ez - ip_list['ip25'])
-    if ip_list['ip33'] % N == 0:
+    if is_valid_inner_product(ip_list['ip33']) == 0:
         f.append(cx * cx + cy * cy + cz * cz - ip_list['ip33'])
-    if ip_list['ip34'] % N == 0:
+    if is_valid_inner_product(ip_list['ip34']) == 0:
         f.append(cx * dx + cy * dy + cz * dz - ip_list['ip34'])
-    if ip_list['ip35'] % N == 0:
+    if is_valid_inner_product(ip_list['ip35']) == 0:
         f.append(cx * ex + cy * ey + cz * ez - ip_list['ip35'])
-    if ip_list['ip44'] % N == 0:
+    if is_valid_inner_product(ip_list['ip44']) == 0:
         f.append(dx * dx + dy * dy + dz * dz - ip_list['ip44'])
-    if ip_list['ip45'] % N == 0:
+    if is_valid_inner_product(ip_list['ip45']) == 0:
         f.append(dx * ex + dy * ey + dz * ez - ip_list['ip45'])
-    if ip_list['ip55'] % N == 0:
+    if is_valid_inner_product(ip_list['ip55']) == 0:
         f.append(ex * ex + ey * ey + ez * ez - ip_list['ip55'])
 
     return f
@@ -94,32 +101,6 @@ def assume_coordinates():
                [ex, ey, ez]]
     return co_list
 
-# def make_formulas(valist, iplist):
-#     ax, ay, az = valist[0:3]
-#     bx, by, bz = vblist[3:6]
-#     cx, cy, cz = vclist[6:9]
-#     dx, dy, dz = vdlist[9:12]
-#     ex, ey, ez = velist[12:15]
-# 
-#     formulas = []
-#     formulas.append(ax)
-#     formulas.append(ay)
-#     formulas.append(az)
-#     formulas.append(by)
-#     formulas.append(bz)
-#     formulas.append(cz)
-#     formulas.append(bx**2 + by**2 + bz**2 - iplist['ip22'])
-#     formulas.append(cx**2 + cy**2 + cz**2 - iplist['ip33'])
-#     formulas.append(dx**2 + dy**2 + dz**2 - iplist['ip44'])
-#     formulas.append(ex * ex + ey * ey + ez * ez - iplist['ip55'])
-#     formulas.append(bx * cx                     - iplist['ip23'])
-#     formulas.append(bx * dx                     - iplist['ip24'])
-#     formulas.append(bx * ex                     - iplist['ip25'])
-#     formulas.append(cx * dx + cy * dy           - iplist['ip34'])
-#     formulas.append(cx * ex + cy * ey           - iplist['ip35'])
-#     formulas.append(dx * ex + dy * ey + dz * ez - iplist['ip45'])
-#     return formulas
-
 def show_answer(answer):
     for i, vertex in enumerate(answer):
         ax, ay, az = vertex[0:3]
@@ -128,25 +109,30 @@ def show_answer(answer):
         dx, dy, dz = vertex[9:12]
         ex, ey, ez = vertex[12:15]
         print('ans[%s]' % i)
-        print('A: %.4s, %.4s, %.4s' % (ax, ay, az))
-        print('B: %.4s, %.4s, %.4s' % (bx, by, bz))
-        print('C: %.4s, %.4s, %.4s' % (cx, cy, cz))
-        print('D: %.4s, %.4s, %.4s' % (dx, dy, dz))
-        print('E: %.4s, %.4s, %.4s' % (ex, ey, ez))
+        print('A: %s, %s, %s' % (ax, ay, az))
+        print('B: %s, %s, %s' % (bx, by, bz))
+        print('C: %s, %s, %s' % (cx, cy, cz))
+        print('D: %s, %s, %s' % (dx, dy, dz))
+        print('E: %s, %s, %s' % (ex, ey, ez))
 
-
-# def _solve(jsonfilename):
-#     
-#     print(jsonfilename)
-#     inner_products = load_inner_product(jsonfilename)
-#     va_list = define_variables()
-#     formulas = make_formulas(va_list, inner_products)
-#     print(formulas)
-#     ans = solve(formulas, va_list)
-# 
-#     show_answer(ans)
+def show_raw_answer(answer):
+    for vertex in answer:
+        alt = {}
+        for k, v in vertex.items():
+            alt[str(k)] = v
+        print('%s, %s, %s' % (alt.get('ax', '@'), alt.get('ay', '@'), alt.get('az', '@')))
+        print('%s, %s, %s' % (alt.get('bx', '@'), alt.get('by', '@'), alt.get('bz', '@')))
+        print('%s, %s, %s' % (alt.get('cx', '@'), alt.get('cy', '@'), alt.get('cz', '@')))
+        print('%s, %s, %s' % (alt.get('dx', '@'), alt.get('dy', '@'), alt.get('dz', '@')))
+        print('%s, %s, %s' % (alt.get('ex', '@'), alt.get('ey', '@'), alt.get('ez', '@')))
 
 def _main(argv):
+
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
+                        filename='tmp.log',
+                        level=logging.DEBUG)
+    print('----------')
+    print('input: length=>[%s], inner_product=>[%s]' % (argv[1], argv[2]))
 
     formulas = []
     co_list = assume_coordinates()
@@ -159,20 +145,20 @@ def _main(argv):
     len_arr = load_length(argv[1])
     f1 = get_formula_with_length(co_list, len_arr)
     formulas.extend(f1)
-    # print('formula_length: %s' % f1)
+    print('formula_length: %s' % f1)
 
     ip_list = load_inner_product(argv[2])
     f2 = get_formula_with_inner_product(co_list, ip_list)
     formulas.extend(f2)
-    # print('formula_inner_product: %s ' % f2)
+    print('formula_inner_product: %s ' % f2)
     
     va_list = [e2 for e1 in co_list for e2 in e1]
-    print('variables:')
-    pprint(formulas)
+    # print('formulas:', end='')
+    # pprint(formulas)
     print('variables: %s' % va_list)
-    ans = solve(formulas, va_list)
+    ans = solve(formulas, va_list, dict=True)
 
-    show_answer(ans)
+    show_raw_answer(ans)
 
 
 if __name__ == '__main__':
@@ -182,22 +168,3 @@ if __name__ == '__main__':
         else:
             _main(sys.argv)
 
-#    _solve('graph501.json')
-#    _solve('graph502.json')
-#    _solve('graph503.json')
-#    _solve('graph504.json')
-#    _solve('graph505.json')
-#    _solve('graph506.json')
-#    _solve('graph507.json')
-#    _solve('graph508.json')
-#    _solve('graph510.json')
-#    _solve('graph511.json')
-#    _solve('graph512.json')
-#    _solve('graph513.json')
-#    _solve('graph515.json')
-#    _solve('graph516.json')
-#    _solve('graph517.json')
-#    _solve('graph518.json')
-#    _solve('graph519.json')
-#    _solve('graph520.json')
-#
