20b68c5f8b6112d2f3adf595dcc0da5785b41728
onion <onion@onionnoMacBook-Air.local>
2017-02-22 13:25:00 +0900

    add comments

--- graph/drawing/relate.py
+++ graph/drawing/relate.py
@@ -7,6 +7,8 @@ import numpy as np
 import logging
 
 def load_length(csvfilename):
+    """ ２頂点間の長さが定義されたCSVファイルを読み込み、5*5の配列にして返す。
+    """
     arr = np.empty((0,5), float)
     with open(csvfilename) as f:
         str_list = csv.reader(f)
@@ -16,6 +18,8 @@ def load_length(csvfilename):
     return arr
 
 def get_formula_with_zero(co_list):
+    """ 頂点v1(a,b,c), 頂点v2(d,e,f), 頂点v3(g,h,i)のa,b,c,e,f,iが0となる式を作る
+    """
     f = []
     index = [[0,0], [0,1], [0,2], [1,1], [1,2],[2,2]]
     for i in index:
@@ -23,6 +27,10 @@ def get_formula_with_zero(co_list):
     return f
 
 def is_valid_length(l):
+    """ 辺の長さが有効な値かどうか判定する
+
+    0.0625で割り切れる時、有効な値としている
+    """
     rtn = True 
     if isinstance(l, float) and l % 0.0625 != 0:
         rtn = False
@@ -31,6 +39,8 @@ def is_valid_length(l):
 
 
 def get_formula_with_length(co_list, len_arr):
+    """ 2頂点間の長さから式を立てる
+    """
     f = []
     index = [[0,1], [0,2], [0,3], [0,4], [1,2],
              [1,3], [1,4], [2,3], [2,4], [3,4]]
@@ -44,6 +54,8 @@ def get_formula_with_length(co_list, len_arr):
     return f
 
 def is_valid_inner_product(ip):
+    """ 内積が有効な値かどうか判定する
+    """
     return True
     rtn = False
     if ip % 0.125 == 0:
@@ -51,6 +63,8 @@ def is_valid_inner_product(ip):
     return rtn
 
 def get_formula_with_inner_product(co_list, ip_list):
+    """ 2頂点間の内積から式を立てる
+    """
     ax, ay, az = co_list[0] 
     bx, by, bz = co_list[1]
     cx, cy, cz = co_list[2]
@@ -83,12 +97,16 @@ def get_formula_with_inner_product(co_list, ip_list):
 
 
 def load_inner_product(filename):
+    """ 内積が定義されたファイルを読み込み、リストとして返す
+    """
     ip = []
     with open(filename) as f:
         ip = json.load(f)
     return ip
 
 def assume_coordinates():
+    """ 連立方程式で使用する文字を宣言する
+    """
     ax, ay, az = symbols('ax ay az', negative=False)
     bx, by, bz = symbols('bx by bz', negative=False)
     cx, cy, cz = symbols('cx cy cz')
@@ -101,21 +119,10 @@ def assume_coordinates():
                [ex, ey, ez]]
     return co_list
 
-def show_answer(answer):
-    for i, vertex in enumerate(answer):
-        ax, ay, az = vertex[0:3]
-        bx, by, bz = vertex[3:6]
-        cx, cy, cz = vertex[6:9]
-        dx, dy, dz = vertex[9:12]
-        ex, ey, ez = vertex[12:15]
-        print('ans[%s]' % i)
-        print('A: %s, %s, %s' % (ax, ay, az))
-        print('B: %s, %s, %s' % (bx, by, bz))
-        print('C: %s, %s, %s' % (cx, cy, cz))
-        print('D: %s, %s, %s' % (dx, dy, dz))
-        print('E: %s, %s, %s' % (ex, ey, ez))
 
 def show_raw_answer(answer):
+    """ 連立方程式の答えを表示する
+    """
     for vertex in answer:
         alt = {}
         for k, v in vertex.items():
@@ -143,6 +150,19 @@ def show_raw_answer(answer):
         print('')
 
 def _main(argv):
+    """ 連立方程式を解くメイン関数
+
+    以下の手順で連立方程式を解く
+    # 01. 連立方程式で使用する変数（代数）を宣言する
+    # 02. 01. で宣言した変数のうち、0に決まるものから立式する
+    # 03. 2頂点間の長さが定義されたファイルを読み込む
+    # 04. 2頂点間の長さから式を立てる
+    # 05. 2頂点間の内積が定義されたファイルを読み込む
+    # 06. 2頂点間の内積から式を立てる
+    # 07. 連立方程式を解く
+    # 08. 答えを表示する
+
+    """
 
     logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
                         filename='tmp.log',
@@ -151,36 +171,49 @@ def _main(argv):
     print('input: length=>[%s], inner_product=>[%s]' % (argv[1], argv[2]))
 
     formulas = []
+    # 
+    # 01. 連立方程式で使用する変数（代数）を宣言する
     co_list = assume_coordinates()
     # print('co_list: %s' % co_list)
 
+    # 02. 01. で宣言した変数のうち、0に決まるものから立式する
     f0 = get_formula_with_zero(co_list)
     formulas.extend(f0)
     # print('formula_zero: %s' % f0)
 
+    # 03. 2頂点間の長さが定義されたファイルを読み込む
     len_arr = load_length(argv[1])
+
+    # 04. 2頂点間の長さから式を立てる
     f1 = get_formula_with_length(co_list, len_arr)
     formulas.extend(f1)
     print('formula_length: %s' % f1)
 
+    # 05. 2頂点間の内積が定義されたファイルを読み込む
     ip_list = load_inner_product(argv[2])
+
+    # 06. 2頂点間の内積から式を立てる
     f2 = get_formula_with_inner_product(co_list, ip_list)
     formulas.extend(f2)
     print('formula_inner_product: %s ' % f2)
     
+    # 07. 連立方程式を解く
     va_list = [e2 for e1 in co_list for e2 in e1]
     # print('formulas:', end='')
     # pprint(formulas)
     print('variables: %s' % va_list)
     ans = solve(formulas, va_list, dict=True)
 
+    # 08. 答えを表示する
     show_raw_answer(ans)
 
 
+# プログラムを実行するとここから処理が始まる
 if __name__ == '__main__':
+  
 
-        if len(sys.argv) != 3:
-            print('usage: python3 ./relate.py [length.csv] [inner_product.json]')
-        else:
-            _main(sys.argv)
+    if len(sys.argv) != 3:
+        print('usage: python3 ./relate.py [length.csv] [inner_product.json]')
+    else:
+        _main(sys.argv)
 
